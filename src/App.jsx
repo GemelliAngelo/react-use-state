5b1d8d7ec0023f8c1cc171018b565304a8f6f1ca
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-02 14:10:41 +0100

    add buttons + style

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,46 @@ function App() {
   return (
     <>
       <header className="header">
-        <h1 className="page-title">Learn Web development</h1>
+        <h1>Learn Web development</h1>
       </header>
-      <main></main>
+      <main>
+        <div className="container">
+          <div className="filters">
+            <ul className="filters-list">
+              <li>
+                <button type="button" className="filters-button">
+                  HTML
+                </button>
+              </li>
+              <li>
+                <button type="button" className="filters-button">
+                  CSS
+                </button>
+              </li>
+              <li>
+                <button type="button" className="filters-button">
+                  Javascript
+                </button>
+              </li>
+              <li>
+                <button type="button" className="filters-button">
+                  Node.js
+                </button>
+              </li>
+              <li>
+                <button type="button" className="filters-button">
+                  Express
+                </button>
+              </li>
+              <li>
+                <button type="button" className="filters-button">
+                  ReactJS
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
       <footer></footer>
     </>
   );
